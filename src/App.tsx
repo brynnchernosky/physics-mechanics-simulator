47a4d43f4147f51e048c495d1ec0f36ae5ca80a6
brynnchernosky <56202540+brynnchernosky@users.noreply.github.com>
2022-10-12 14:38:44 -0400

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="mechanicsSimulationContainer">test</div>;
+  const [simulationElements, setSimulationElements] = useState<JSX.Element[]>(
+    []
+  );
+
+  const addWeight = () => {
+    const weight = <div className="weight" key={simulationElements.length} />;
+    console.log(simulationElements);
+    setSimulationElements((state) => [...state, weight]);
+  };
+
+  return (
+    <div className="mechanicsSimulationContainer">
+      <div className="mechanicsSimulationContentContainer">
+        <div className="mechanicsSimulationButtonsAndElements">
+          <div className="mechanicsSimulationButtons">
+            <button
+              onClick={addWeight}
+              disabled={simulationElements.length > 0}
+            >
+              {" "}
+              Add element
+            </button>
+            <button> Settings</button>
+          </div>
+          <div className="mechanicsSimulationElements">
+            {simulationElements}
+          </div>
+        </div>
+        <div className="mechanicsSimulationFooter">
+          <button> Start</button>
+          <button> Pause</button>
+          <button> Reset</button>
+          <button> Slider</button>
+        </div>
+      </div>
+      <div className="mechanicsSimulationEquationContainer">
+        <div className="mechanicsSimulationEquation">
+          Position: x_1=x_0+vt+at^2
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
